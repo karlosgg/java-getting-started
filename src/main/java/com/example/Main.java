e12ad3362f1fd46aefeaeaccf5f1150c06b997ab
lubali <karlosgg@gmail.com>
2019-08-10 21:35:51 -0600

    Removing bootstrap css

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,119 +16,16 @@
 
 package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.sql.DataSource;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
 
 @Controller
 @SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Autowired
-  private ApplicationContext ctx;
-
-  @Autowired
-  private DataSource dataSource;
-
-  @Autowired
-  private ResourceLoader resourceLoader;
-
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args){
     SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index(Model model) {
-    String str = "Hello";
-    BufferedWriter writer = null;
-    try {
-      writer = new BufferedWriter(new FileWriter(System.getProperty("java.io.tmpdir")+"/texto.txt"));
-      writer.write(str);
-      writer.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    Resource template = ctx.getResource(System.getProperty("java.io.tmpdir")+"/texto.txt");
-    model.addAttribute("ctx", template);
-    model.addAttribute("tmpDir", System.getProperty("java.io.tmpdir"));
-
-    String employees="";
-    File resource = null;
-    Resource res = resourceLoader.getResource("file:"+System.getProperty("java.io.tmpdir")+"/texto.txt");
-    File file=null;
-    String s=">";
-
-    try {
-      file = res.getFile();
-      s = new String(Files.readAllBytes(file.toPath()));
-      //resource = template.getFile();
-      //employees = new String(
-        //      Files.readAllBytes(resource.toPath()));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    model.addAttribute("cadena", s);
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
   }
 
 }
